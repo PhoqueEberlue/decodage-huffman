b14f441c00ed96df05f38760f1ed55da8744076f
Andrew Mary Huet de Barochez <phoqueeberlue@gmail.com>
2022-05-11 19:35:40 +0200

    added the huffman file that will be used to call the huffman functions

---
+++ src/huffman_decoding.cpp
@@ -0,0 +1,10 @@
+//
+// Created by andrewmhdb on 08/05/22.
+//
+
+#include "huffman_decoding.h"
+
+void mainDecode(const std::string& filePath) {
+    std::unique_ptr<Alphabet> alphabet(new Alphabet());
+    alphabet->generateAlphabet(filePath);
+}
---
+++ src/huffman_decoding.h
@@ -0,0 +1,14 @@
+//
+// Created by andrewmhdb on 08/05/22.
+//
+
+#ifndef DECODAGE_HUFFMAN_HUFFMAN_DECODING_H
+#define DECODAGE_HUFFMAN_HUFFMAN_DECODING_H
+
+#include "alphabet.h"
+#include <string>
+#include <memory>
+
+void mainDecode(const std::string& filePath);
+
+#endif //DECODAGE_HUFFMAN_HUFFMAN_DECODING_H
